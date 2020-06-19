b62bc3269c7f9685f950680499c3490f6521633c
Pietro Di Bello <pierodibello@gmail.com>
2020-06-19 12:40:39 +0200

    say Buzz when the number is 5

--- src/test/kotlin/fizzbuzz/FizzBuzzTest.kt
+++ src/test/kotlin/fizzbuzz/FizzBuzzTest.kt
@@ -7,15 +7,27 @@ class FizzBuzzTest {
 
     @Test
     fun `say 1 when number is 1`() {
-        val said = FizzBuzz().say(1)
-
-        assertEquals("1", said)
+        assertEquals("1", FizzBuzz().say(1))
     }
 
     @Test
     fun `say 2 when number is 2`() {
-        val said = FizzBuzz().say(2)
-
-        assertEquals("2", said)
+        assertEquals("2", FizzBuzz().say(2))
     }
+
+    @Test
+    fun `say 4 when number is 4`() {
+        assertEquals("4", FizzBuzz().say(4))
+    }
+
+    @Test
+    fun `say Fizz when number is 3`() {
+        assertEquals("Fizz", FizzBuzz().say(3))
+    }
+
+    @Test
+    fun `say Buzz when number is 5`() {
+        assertEquals("Buzz", FizzBuzz().say(5))
+    }
+
 }
