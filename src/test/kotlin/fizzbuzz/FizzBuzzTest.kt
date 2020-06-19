791203ad0b6f386261ef8015b9f18fbbc99257ce
Alessio Coser <alessio.coser@xpeppers.com>
2020-06-19 15:09:31 +0200

    try to follow doubleloop presentation

--- src/test/kotlin/fizzbuzz/FizzBuzzTest.kt
+++ src/test/kotlin/fizzbuzz/FizzBuzzTest.kt
@@ -11,48 +11,23 @@ class FizzBuzzTest {
     )
 
     @Test
-    fun `say 1 when number is 1`() {
+    fun `say the number when there are no rule matching`() {
         assertEquals("1", FizzBuzz(rules).say(1))
-    }
-
-    @Test
-    fun `say 2 when number is 2`() {
         assertEquals("2", FizzBuzz(rules).say(2))
     }
 
     @Test
-    fun `say 4 when number is 4`() {
-        assertEquals("4", FizzBuzz(rules).say(4))
-    }
-
-    @Test
-    fun `say Fizz when number is 3`() {
+    fun `say the matched word when there is only one rule matching`() {
         assertEquals("Fizz", FizzBuzz(rules).say(3))
-    }
-
-    @Test
-    fun `say Buzz when number is 5`() {
+        assertEquals("Fizz", FizzBuzz(rules).say(3 * 2))
         assertEquals("Buzz", FizzBuzz(rules).say(5))
+        assertEquals("Buzz", FizzBuzz(rules).say(5 * 2))
     }
 
     @Test
-    fun `say Fizz when number is 6`() {
-        assertEquals("Fizz", FizzBuzz(rules).say(6))
-    }
-
-    @Test
-    fun `say Buzz when number is 10`() {
-        assertEquals("Buzz", FizzBuzz(rules).say(10))
-    }
-
-    @Test
-    fun `say FizzBuzz when number is 15`() {
-        assertEquals("FizzBuzz", FizzBuzz(rules).say(15))
-    }
-
-    @Test
-    fun `say FizzBuzz when number is 30`() {
-        assertEquals("FizzBuzz", FizzBuzz(rules).say(30))
+    fun `concatenate all matched words when there are many rules matching`() {
+        assertEquals("FizzBuzz", FizzBuzz(rules).say(3 * 5))
+        assertEquals("FizzBuzz", FizzBuzz(rules).say(3 * 5 * 2))
     }
 
     @Test
