584db0021335a9b38c430a2c683515f3ef6f74bf
Alessio Coser <alessio.coser@xpeppers.com>
2020-06-25 11:30:19 +0200

    say the plain number

--- src/test/kotlin/fizzbuzz/FizzBuzzTest.kt
+++ src/test/kotlin/fizzbuzz/FizzBuzzTest.kt
@@ -19,4 +19,16 @@ class FizzBuzzTest {
     fun `say Fizz for the number 3`() {
         assertEquals("Fizz", FizzBuzz().say(3))
     }
+
+    @Test
+    fun `say 4 for the number 4`() {
+        assertEquals("4", FizzBuzz().say(4))
+    }
+
+    @Test
+    fun `say the plain number`() {
+        assertEquals("1", FizzBuzz().say(1))
+        assertEquals("2", FizzBuzz().say(2))
+        assertEquals("4", FizzBuzz().say(4))
+    }
 }
