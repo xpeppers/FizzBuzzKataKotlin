af117229325866ca0ecf2b118f253754c4074264
Alessio Coser <alessio.coser@xpeppers.com>
2020-06-25 11:25:07 +0200

    say Fizz for the number 3

--- src/test/kotlin/fizzbuzz/FizzBuzzTest.kt
+++ src/test/kotlin/fizzbuzz/FizzBuzzTest.kt
@@ -14,4 +14,9 @@ class FizzBuzzTest {
     fun `say 2 for the number 2`() {
         assertEquals("2", FizzBuzz().say(2))
     }
+
+    @Test
+    fun `say Fizz for the number 3`() {
+        assertEquals("Fizz", FizzBuzz().say(3))
+    }
 }
