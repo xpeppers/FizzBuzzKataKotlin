6d34fd9ab2f15ec03ff6526cdd418583f1b35732
Alessio Coser <alessio.coser@xpeppers.com>
2020-06-25 11:31:42 +0200

    remove duplicated test

--- src/test/kotlin/fizzbuzz/FizzBuzzTest.kt
+++ src/test/kotlin/fizzbuzz/FizzBuzzTest.kt
@@ -6,29 +6,14 @@ import org.junit.Test
 class FizzBuzzTest {
 
     @Test
-    fun `say 1 for the number 1`() {
+    fun `say the plain number`() {
         assertEquals("1", FizzBuzz().say(1))
-    }
-
-    @Test
-    fun `say 2 for the number 2`() {
         assertEquals("2", FizzBuzz().say(2))
+        assertEquals("4", FizzBuzz().say(4))
     }
 
     @Test
     fun `say Fizz for the number 3`() {
         assertEquals("Fizz", FizzBuzz().say(3))
-    }
-
-    @Test
-    fun `say 4 for the number 4`() {
-        assertEquals("4", FizzBuzz().say(4))
-    }
-
-    @Test
-    fun `say the plain number`() {
-        assertEquals("1", FizzBuzz().say(1))
-        assertEquals("2", FizzBuzz().say(2))
-        assertEquals("4", FizzBuzz().say(4))
     }
 }
